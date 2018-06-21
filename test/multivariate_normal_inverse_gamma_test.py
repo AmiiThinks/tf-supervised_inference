Fix covariance computation.

test/multivariate_normal_inverse_gamma_test.py
@@ -60,10 +60,10 @@ class MultivariateNormalInverseGammaTest(tf.test.TestCase):
         y = np.random.normal(0, 1, [10, 1]).astype('float32')
         patient = patient.next(x, y)
 
-        self.assertAllClose([[-0.201125], [-0.109042]], patient.sample())
-        self.assertAllClose([[-0.03634], [-0.015134]],
+        self.assertAllClose([[-0.201125], [-0.117803]], patient.sample())
+        self.assertAllClose([[-0.03634], [-0.094946]],
                             patient.sample())
-        self.assertAllClose([[-0.215406], [0.163388]], patient.sample())
+        self.assertAllClose([[-0.215406], [0.160784]], patient.sample())
 
 
 if __name__ == '__main__':

tf_supervised_inference/distributions.py
@@ -52,12 +52,10 @@ class MultivariateNormal(object):
             unscaled_means += normal_prior.weighted_precision_sums
 
         L = tf.cholesky(self.precision)
-        U = tf.transpose(L)
-        self.means = tf.matrix_triangular_solve(
-            U, tf.matrix_triangular_solve(L, unscaled_means), lower=False)
+        self.means = tf.cholesky_solve(L, unscaled_means)
 
         self.covariance_scale = tf.matrix_triangular_solve(
-            U, tf.eye(L.shape[0].value), lower=False)
+            L, tf.eye(L.shape[0].value))
 
         self.weighted_precision_sums = self.precision @ self.means
         self.quadratic_form = tf.matmul(