5e22c60ebdf2fc8cd706327eb39c7dd9f8f4ea26
Dustin Morrill <dmorrill10@gmail.com>
2018-06-22 11:24:25 -0600

    Version bump.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='tf_supervised_inference',
-    version='0.0.2',
+    version='0.1.0',
     license='',
     packages=find_packages(),
     install_requires=[
         'future >= 0.15.2',
         'setuptools >= 20.2.2',
         # tensorflow or tensorflow-gpu >= 1.8
-        'fire'
+        # fire for private executables in bin.
     ],
     tests_require=['pytest', 'pytest-cov'],
     setup_requires=['pytest-runner'],
